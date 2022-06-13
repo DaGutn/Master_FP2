dbccc73043f1ee7a9ec7b26899851666a80ef39a
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-06-13 11:31:46 +0200

    AFM erstellt

---
+++ AFM/plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp 
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from pylab import *
