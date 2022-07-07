3bd7a714cdcb12882391ca99190bc8c4109bd338
MiraArndt <mira.arndt@tu-dortmund.de>
2022-07-07 18:16:18 +0200

    Auswertung und Diskussion

--- AFM/plot.py
+++ AFM/plot.py
@@ -5,3 +5,8 @@ from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from pylab import *
+
+Spurabstand = ufloat (1.52,0.002)*10**(-6)
+Pitlaenge = ufloat (0.78,0.02)*10**(-6)
+
+print ((4/(Spurabstand*Pitlaenge))*0.0089/17)
